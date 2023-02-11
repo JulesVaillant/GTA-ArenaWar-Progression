eea7bd2f41c791ee513d781e3e580840c4fa491f
Khopylium <34626855+Khopylium@users.noreply.github.com>
2023-02-11 12:05:21 +0100

    default values for "level" and "APperHour" added

--- GTA_ArenaWar_Progression.py
+++ GTA_ArenaWar_Progression.py
@@ -1,7 +1,7 @@
 def ArenaTotal(level):
     return 5*level*level + 45*level
      
-def HoursLeft(APperHour=3000, level=0):
+def HoursLeft(APperHour, level):
     hours = []
     for i in range(len(RewardsList)):
         hours.append((ArenaTotal(LevelsList[i]) - ArenaTotal(level))/APperHour)
@@ -14,17 +14,21 @@ RewardsList = ["Taxi", "Dozer", "Clown Van", "Trashmaster", "Barracks Semi", "Mi
 LevelsList = [25,50,75,100,200,300,500,1000]
 
 if __name__ == '__main__':
-    level = input("What level are you? ")
-    APperHour = input("How much AP per hour do you make? (3k by default)")
-    if(APperHour == ''):
+    try:
+        level = int(input("What level are you? (0 by default)"))
+    except ValueError:
+        level = 0
+    try:
+        APperHour = int(input("How much AP per hour do you make? (3k by default)"))
+    except ValueError:
         APperHour = 3000
-    HoursList = HoursLeft(int(APperHour), int(level))
-    print("You got: " + str(ArenaTotal(int(level))) + " AP")
+    HoursList = HoursLeft(APperHour, level)
+    print("You got: " + str(ArenaTotal(level)) + " AP")
     print('-' * 60)
     print('{:<20s}{:>1s}{:>18s}'.format("REWARD","COMPLETION (%)", "HOURS LEFT"))
     print('-' * 60)
     for i in range(len(RewardsList)):
-        completion = ArenaTotal(int(level)) / ArenaTotal(LevelsList[i])*100  #completion of reward in %
+        completion = ArenaTotal(level) / ArenaTotal(LevelsList[i])*100  #completion of reward in %
         rounded = round(completion,2)
         if(rounded > 100):
             rounded = 100

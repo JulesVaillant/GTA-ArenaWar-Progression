d34951f2eef42327f7fc2f163491511bb3826201
Khopylium <atomium.boy@gmail.com>
2024-03-09 12:49:01 +0100

    Cleaned code

--- GTA_ArenaWar_Progression.js
+++ GTA_ArenaWar_Progression.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const RewardsList = ["Taxi", "Dozer", "Clown Van", "Trashmaster", "Barracks Semi", "Mixer", "Space Docker", "Tractor"];
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index', { progressValue: 99 });
+    res.render('index', { RewardsList: RewardsList});
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,10 +1,21 @@
 const RewardsList = ["Taxi", "Dozer", "Clown Van", "Trashmaster", "Barracks Semi", "Mixer", "Space Docker", "Tractor"]
 const LevelsList = [25,50,75,100,200,300,500,1000]
 
+/**
+ * Function that returns the total number of AP acquired at the specified level  
+ * @param {Number} level 
+ * @returns {Number} total number of AP acquired at the specified level
+ */
 function arenaTotal(level){
     return 5*level**2 + 45*level 
 }
 
+/**
+ * Function that returns how many hours of grind left based on the specified level and the specified amount of AP per hour 
+ * @param {Number} APperHour 
+ * @param {Number} currentLevel 
+ * @returns {Number[]} array containing the hours left to grind for each reward
+ */
 function hoursLeft(APperHour, currentLevel){
     var hoursList=[]
     LevelsList.forEach((levelToReach)=>{
@@ -16,6 +27,11 @@ function hoursLeft(APperHour, currentLevel){
     return hoursList
 }
 
+/**
+ * Function that returns all the completion purcentages for all the rewards
+ * @param {Number} currentLevel 
+ * @returns {Number[]} array containing the purcentages of completions for each reward
+ */
 function purcentageCompleted(currentLevel){
     var purcentagesList = []
     LevelsList.forEach((levelToReach)=>{
@@ -25,6 +41,7 @@ function purcentageCompleted(currentLevel){
     });
     return purcentagesList;
 }
+
 
 function updateProgressBars() {
     var inputLevel = document.getElementById("inputLevel").value;
